f91bf7be70aff0e7411838ef0baa5395e93d4c7f
Cameron Chandler <webstormcam@gmail.com>
2021-08-31 12:06:18 -0400

    Was able to the bottom piece appear on click on the large card

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function myFunction(){
     var x = document.getElementById("mobile_click");
     if(x.style.display ==='none'){
-        x.style.display = 'block';
+        x.style.display = 'flex';
     } else{
         x.style.display = 'none'
     }
